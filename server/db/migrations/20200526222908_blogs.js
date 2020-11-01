d4222f3754ce2fabc30f430ff442b793fb73320b
Ben Halverson <benhalverson33@gmail.com>
2020-10-31 21:37:51 -0700

    linting updates

--- server/db/migrations/20200526222908_blogs.js
+++ server/db/migrations/20200526222908_blogs.js
@@ -1,6 +1,5 @@
-
-exports.up = function (knex, Promise) {
-  return knex.schema.createTable('blogs', (table) => {
+exports.up = (knex) => {
+  knex.schema.createTable('blogs', (table) => {
     table.increments();
     table.string('author').notNullable();
     table.string('title').notNullable();
@@ -8,6 +7,6 @@ exports.up = function (knex, Promise) {
   });
 };
 
-exports.down = function (knex, Promise) {
-  return knex.schema.dropTable('blogs');
+exports.down = (knex) => {
+  knex.schema.dropTable('blogs');
 };
